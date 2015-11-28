00074ee5b90b7561272b8b5d350285c85916f77a
Lance Maverick <pnoel6@gmail.com>
2015-11-28 18:21:32 +0000

    added better comments and cleaned up

--- skybeard/beard_functions.py
+++ skybeard/beard_functions.py
@@ -4,8 +4,8 @@ import events
 import telegram
 import re
 import random
-from dota_functions import *
-from msg_texts import *
+import dota_functions as df
+from msg_texts import * #imports 'msgs' dictionary
 import pdb
 import numpy as np
 import matplotlib
@@ -17,39 +17,48 @@ import pyowm
 import omdb
 import logging
 import math
-logging.basicConfig(filename='botlog.log',
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+import datetime
+import sys
+import os
 
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',stream=sys.stdout, level=logging.INFO)
 logging.getLogger().addHandler(logging.StreamHandler())
+
 #Request, format and send dota last match info
 def last_match(bot, message):
     sendText(bot,message.chat_id,msgs['getmatch'])
+    
     try:
-        dota_id = get_dota_id_from_telegram(message.from_user.first_name)
-        logging.info('Last match request:',dota_id,message.from_user.first_name)
-        match_id = getLastMatch(dota_id)
+        dota_id = df.get_dota_id_from_telegram(message.from_user.id)
+        logging.info('Last match request:',dota_id,message.from_user.first_name,dota_id,message.from_user.id)
+        match_id = df.getLastMatch(dota_id)
         bot.sendMessage(chat_id=message.chat_id,
             text="[Requested DotaBuff page for match "
             +str(match_id)+"](http://dotabuff.com/matches/"+str(match_id)+").",
             parse_mode=telegram.ParseMode.MARKDOWN)
     except:
+        logging.info('last match info requested my non-registered user,',message.user)
         bot.sendMessage(chat_id=message.chat_id,text='You don\'t appear to be in the catabase. Please register using the /register command')
 
 #Request, format and send dota feeding info.
-#Todo: Cache results and update weekly or when requested
-def feeding(bot,message,update=False):
-    #pdb.set_trace()
+def feeding(bot,message,BASE_URL,update=False):
     sendText(bot,message.chat_id,msgs['getfeed'])
     
     table = msgs['feedtable']
-    if (update):
-        sendText(bot,message.chat_id,msgs['serverpoll'])
-        feeds = valRank('deaths')
-        pickle.dump(feeds,open('catabase/feeds.p','wb'))
     
+    #refreshed data from dota api
+    if (update):
+        logging.info('feed data update requested')
+        sendText(bot,message.chat_id,msgs['serverpoll'])
+        feeds = df.valRank('deaths')
+        pickle.dump(feeds,open('catabase/feeds.p','wb'))
+        logging.info('new feeds file saved')
+    
+    #displays cached data
     else:
         try:
             feeds = pickle.load(open('catabase/feeds.p','rb')) 
+        #will update automatically if local cache not found
         except Exception as e:
             logging.error(e)
             sendText(bot,message.chat_id,'Local feed data not found...')
@@ -57,7 +66,8 @@ def feeding(bot,message,update=False):
             feeds = valRank('deaths')
             logging.info('feeding data defaulted to:', feeds)
             pickle.dump(feeds,open('catabase/feeds.p','wb'))
-    i=0;
+    
+    i=0; #rank iterator
     for rank in feeds:
         i+=1
         table+=str(i)+"....."+str(rank['dota_name'])+"...."+str(rank['total_vals'])+"\n"    
@@ -95,9 +105,10 @@ def feeding(bot,message,update=False):
     
     return imgPath 
 
-#Wrapper for telegram.bot.sendMessage() function
+#Wrapper for telegram.bot.sendMessage() function. Markdown enabled
 def sendText(bot,chat_id,text,webprevoff=False):
     bot.sendMessage(chat_id=chat_id,text=text,parse_mode="Markdown",disable_web_page_preview=webprevoff)
+    #bot.sendMessage(chat_id=chat_id,text=text,disable_web_page_preview=webprevoff)
 
 #Searches for commands sent to bot
 def command(cmd,text):
@@ -113,7 +124,7 @@ def keywords(words,text):
     else:
         return False
 
-#for printing to terminal. Switching to logging module instead
+
 def infoprint(bot,message,text):
     logging.info(("\nDota info request:\n["+text+"]"))
     logging.info("USER: ",message.from_user.id,message.from_user.first_name) 
@@ -122,13 +133,17 @@ def infoprint(bot,message,text):
 
 #Show chat members why they should go lift
 def gainz(bot,chat_id,message):
+    
     gain_photos= [
             'http://i.imgur.com/QrdcYCP.jpg',
             'http://i.imgur.com/zopXdvc.jpg',
             'http://i.imgur.com/jEi18ha.jpg'
             ]
     i = random.randint(0, len(gain_photos)-1)
-    bot.sendPhoto(chat_id=chat_id, photo=gain_photos[i])
+    try:
+        bot.sendPhoto(chat_id=chat_id, photo=gain_photos[i])
+    except:
+        logging.error('unable to parse photo:',cat_photos[i],sys.exc_info()[0])
 
 
 #Post cats in space
@@ -150,7 +165,7 @@ def postCats(bot,message):
     try:
         bot.sendPhoto(chat_id=message.chat_id, photo=cat_photos[i])
     except:
-        print cat_photos[i]
+        logging.error('unable to parse photo:',cat_photos[i],sys.exc_info()[0])
         bot.sendPhoto(chat_id=message.chat_id,photo='http://cdn.meme.am/instances/500x/55452028.jpg')
 
 
@@ -162,7 +177,7 @@ def postImage(imagePath,chat_id,REQUEST_URL):
     try:
         files = {'photo': open(imagePath,'rb')}
     except:
-        return
+        return logging.error('could not send local photo',sys.exc_info()[0])
     return requests.post(REQUEST_URL + '/sendPhoto', params=data, files=files)
     
 #possibility of uploading pics to dropbox with this function (needs python 2.7 or greater)    
@@ -181,8 +196,11 @@ def postImage(imagePath,chat_id,REQUEST_URL):
 #    out.close()
 #    print metadata
 
+#search the omdb for a movie and displays the poster and summary
 def movies(bot,message,title):
     chat_id=message.chat_id
+    
+    #makes the imdb search url if match not found
     def buildImdbUrl(title):
         title_list = [element.strip() for element in title.split(' ')]
         url_elements = [
@@ -193,8 +211,8 @@ def movies(bot,message,title):
         url = ''.join(url_elements)
         logging.info('imdb url built',url)
         return url
-        
     
+    #poll api
     try:
         result = omdb.get(title=title)
         result.title
@@ -221,53 +239,60 @@ def movies(bot,message,title):
                 'IMDb rating: '+imdbscore+'\n'
                 'Plot:\n'+plot
                 )     
+
+        #exception handling for movies with no poster   
         try:
             bot.sendPhoto(chat_id=chat_id,photo=poster)
         except:
-            logging.error("no poster found",result)
+            logging.error("no poster found in omdb result",result)
         sendText(bot,chat_id,reply)
         sendText(bot,chat_id,imdb,True)
 
 
-#python 2.7+ only
 #gives current weather and forecast for given location.
-def forecast(bot,message):
-    
+def forecast(bot,message,BASE_URL):
     owm = pyowm.OWM(os.environ.get('OWM_TOKEN'))
     text = message.text
     
     try:
         location = text.split('/weather',1)[1]
+    
+    #default location
     except:
         location = 'Birmingham,uk'
     if not location:
         location = 'Birmingham,uk'
     try:    
+        #7 days of forecast
         forecast = owm.daily_forecast(location,limit=7)
         tomorrow = pyowm.timeutils.tomorrow()
         weather_tmrw = forecast.get_weather_at(tomorrow)
+        logging.info(tomorrow)
     except:
+        logging.info('weather request error ',sys.exc_info()[0])
         return sendText(bot,message.chat_id,'I\'m sorry, something went wrong.')
-        
+    
+    #api gives 'closest match'. bot prints true location name
     true_location = forecast.get_forecast().get_location().get_name()
     coor_lon =  forecast.get_forecast().get_location().get_lon()
     coor_lat =  forecast.get_forecast().get_location().get_lat()
-
     
     lst_clouds = []
     lst_temp = []
     lst_status = []
     lst_time = []
-    
 
     f_day = forecast.get_forecast()
+    
+    #get forecasts for each day
     for weather in f_day:
         lst_clouds.append( weather.get_clouds())
         lst_temp.append(weather.get_temperature('celsius')['max'])
         lst_status.append(weather.get_status())
         lst_time.append(weather.get_reference_time('iso'))
-    print lst_clouds,lst_temp,lst_status,lst_time,len(lst_temp)
+    logging.info(lst_clouds,lst_temp,lst_status,lst_time,len(lst_temp))
 
+    #7 day forecast plot 
     fig = plt.figure()
     ax = fig.add_subplot(111)
     N= len(lst_status)
@@ -275,8 +300,6 @@ def forecast(bot,message):
     width =0.6
     bars_temp = ax.bar(ind, lst_temp, width,
                             color='#e2e6ed')
-#    bars_cloud = ax.bar(ind, lst_temp, width,
-#                            color='blue')
     ax.set_axis_bgcolor('#bcc5d4')
     ax.set_xlim(-width,len(ind)+width)
     ax.set_ylim(0,max(lst_temp)+5)
@@ -290,30 +313,40 @@ def forecast(bot,message):
     imgPath = 'img/weather.png'
     plt.savefig(imgPath,facecolor='#bcc5d4')
     temp_tmrw = weather_tmrw.get_temperature('celsius')
-    
+   
+    #check each status (quick implementation. Needs to be mapped)    
     if forecast.will_be_sunny_at(tomorrow):
+        logging.info('status: sunny')
         status = 'sunny'
     elif forecast.will_be_sunny_at(tomorrow):
+        logging.info('status: cloudy')
         status = 'cloudy'
     elif forecast.will_be_rainy_at(tomorrow):
+        logging.info('status: rainy')
         status = 'rainy'
     elif forecast.will_be_snowy_at(tomorrow):
+        logging.info('status: snowy')
         status = 'snowy'
     elif forecast.will_be_stormy_at(tomorrow):
+        logging.info('status: stormy')
         status = 'stormy'
     elif forecast.will_be_foggy_at(tomorrow):
+        logging.info('status: foggy')
         status = 'foggy'
     elif forecast.will_be_tornado_at(tomorrow):
+        logging.info('status: tornado')
         status = 'TORNADO!!'
     elif forecast.will_be_hurricane_at(tomorrow):
+        logging.info('status: hurricane')
         status = 'HURRICANE!!'
     else:
+        logging.error('unknown weather status',forecast.get_forecast)
         status = '<unknown status>'
     
     logging.info('forecast request:',message,forecast) 
     fore_reply = 'Tomorrow in '+true_location+', it will be *'+status+'* with a maximum temperature of *'+str(temp_tmrw['max'])+'* degrees C' 
     
-
+    #current weather observation   
     observation = owm.weather_at_place(location)
     weather = observation.get_weather()
     logging.info('weather results:',message,weather)
@@ -322,14 +355,18 @@ def forecast(bot,message):
 
     obs_reply = 'Current weather status in *'+true_location+': ' +str(weather.get_detailed_status())+'*.\nWind speed (km/h):\t*'+str(obs_wind['speed'])+'*. Temperature (C):\t*'+str(obs_temp['temp'])+'*'
     
+    #current weather and forecast replies
     sendText(bot,message.chat_id,obs_reply)
-
     sendText(bot,message.chat_id,fore_reply)
-    
+    #location of weather report
     bot.sendLocation(message.chat_id,coor_lat,coor_lon)
+    #send forecast graph
+    postImage(imgPath,message.chat_id,BASE_URL) 
+     
 
-    return imgPath
-
+#calcuate the distance between two points on the surface of a sphere.
+#returns true if distance of location two is within given radius from location two
+#takes two sets of map coordinates and radius 
 def haversine(lat1,lon1,lat2,lon2,radius):
 
     earth_radius = 6371
@@ -342,36 +379,44 @@ def haversine(lat1,lon1,lat2,lon2,radius):
     c = cos = (math.sin(phi1)*math.sin(phi2)*math.cos(theta1-theta2)+math.cos(phi1)*math.cos(phi2))
     d = math.acos(c)*earth_radius
     
+    logging.info('location check. Distance: ',d )
+    
     if d < radius:
-        return True
+        return d,True
     else:
-        return False
+        return d,False
 
 
     
-
+#checks if user location is within the vicinity of location of interest
+#currently checks is location is within 100m of particular gyms
 def locCheck(bot,message): 
 
     lon = message.location.longitude
     lat = message.location.latitude
-    gyms = yaml.load(open('catabase/gyms.yaml','rb'))
+    try:
+        gyms = yaml.load(open('catabase/gyms.yaml','rb'))
+    except:
+        gyms = []
+        logging.error('No gym coordinates found')
+
     name = message.from_user.first_name
 
     for gym in gyms:
-        if haversine(gym['lat'],gym['lon'],lat,lon,0.1):
+        d,in_radius = haversine(gym['lat'],gym['lon'],lat,lon,0.1)
+        if in_radius:
+            logging.info('in gym vicinity ',gym['lat'],gym['lon'],lat,lon)
             return sendText(bot,message.chat_id,'Good job on the gains at ' +gym['name']+' '+name+', I\'m proud of you')
-        else:
-            return
-            
 
-
+ 
+#thank user
 def thank(bot,chat_id,message):
 
     sendText(
             bot,chat_id,
             ' '.join([msgs['thanks'],str(message.from_user.first_name)]))
 
-#greet users
+#greet user
 def greet(bot,chat_id,message):
     sendText(
             bot,chat_id,
@@ -379,12 +424,83 @@ def greet(bot,chat_id,message):
                 str(message.from_user.id),
                 msgs['welcome2'],
                 message.from_user.first_name]))
-    
+
+
 #say goodbye to users
 def goodbye(bot,chat_id,message):
     sendText(bot,chat_id,"Daisy...daisy...") 
 
+
+#echo user message to given chat id    
 def echocats(bot,message):
     echo = message.text.split('/echo',1)[1]
-    sendText(bot,-17644459,echo)
+    chat = yaml.load(open('catabase/echochat.yaml','rb'))
+    try:
+        sendText(bot,chat,echo)
+    except:
+        return logging.error('failed to echo message',message,echo,chat)
+
+
+#get latest dota news post
+def dotaNews(bot,message):
+
+    payload={
+            'appid':'570',  #dteam appid for dota2
+            'count':'1',    #latest news post only
+            'maxlength':'300' #maximum length of news summary for bot message
+            }
+    news = df.steamNews(payload)[0]
+    
+    logging.info(news)
+    
+    if not news:
+        logging.error('No news items found in steam api request')
+        return sendText(bot,message.chat_id,msgs['nonews'])
+    
+    header = 'Latest Dota 2 news post ('+news['feedlabel']+')'
+    
+    try:
+        return bot.sendMessage(chat_id=message.chat_id,text=newsReply(header,news),disable_web_page_preview=True)
+    except:
+        logging.error('Couldn\'t parse: ',news)
+ 
+#format news message for bot message
+def newsReply(header,news):
+    title = news['title']
+    contents = news['contents']
+    url = news['url']
+    texts = [
+            header,
+            title,
+            '\n',
+            re.sub('[*]', '', contents),#* causes problems with message parsing with markdown enabled
+            '\n',
+            url]
+    reply = '\n'.join(texts)
+    print reply
+    return reply
+
+#post new dota 2 patch
+def postUpdate(bot,message):
+    patch = df.getNewPatch()
+    if patch:
+        date = datetime.datetime.fromtimestamp(
+            patch['date']
+            ).strftime('%Y-%m-%d %H:%M')
+                   
+        header = '*NEW DOTA 2 PATCH!*'+date+''
+        try:
+            #using sendMessage as markdown can cause problems
+            bot.sendMessage(chat_id=message.chat_id,text=newsReply(header,patch),disable_web_page_preview=True)
+        except:
+            logging.error('Couldn\'t parse: ',patch)
+        #update list of potentially unpatched players from the catabase
+        cats = yaml.load(open('catabase/catabase.yaml','rb'))
+        yaml.dump(cats,open('catabase/unpatched.yaml','wb'))
+
+#checks if something has expired, e.g a dota event or the time between dota 2 update checking
+def expiry(initial_time,dtime): #dtime in hours
+    if datetime.datetime.now() > initial_time+datetime.timedelta(hours=dtime):
+        return True
+
 
