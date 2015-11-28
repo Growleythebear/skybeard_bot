51d412e4168d01be10b8445477f0ebf4f58d0161
Lance Maverick <pnoel6@gmail.com>
2015-11-28 18:21:51 +0000

    added better comments and cleaned up

--- skybeard/dota_functions.py
+++ skybeard/dota_functions.py
@@ -1,6 +1,5 @@
 #Functions for interfacting with DOTA 2 API. Uses the thin wrapper Dota2py by Andrew Snowden for requests
 #This is very quick and dirty 5am code do test requesting from DOTA 2 API. 
-
 from dota2py import api
 import sys
 from os.path import abspath, join, dirname
@@ -8,6 +7,10 @@ import os
 from datetime import date, timedelta
 import requests
 import register as reg
+import bottools as btools
+import yaml
+import logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',stream=sys.stdout, level=logging.INFO)
 #Spacecats dictionary for steam id and name lookup.
 #All functions currently use first_name value from telegram message.
 #All functions are agnostic of which identifier is used. message.from_user.id is safer,
@@ -15,15 +18,55 @@ import register as reg
 
 
 
+#get news posts from steam api
+def steamNews(payload):
+    
+    steam_url = 'http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/'
+    try:
+        response = requests.get(steam_url,params=payload)
+        news = response.json()['appnews']
+        return news['newsitems']
+    except:
+        logging.error('no news items found')
+        return None
+
+#check for new dota 2 patch from steam api
+def getNewPatch():
+    
+    last_update = yaml.load(open('catabase/d2_patch_id.yaml','rb'))
+    
+    payload={
+            'appid':'570',
+            'count':'20',
+            'maxlength':'300'
+            }
+    news = steamNews(payload)
+    patch = btools.keySearch(news,'feedname','steam_updates')
+    print patch
+    if patch:
+        patch_id = patch['gid']
+    else:
+        return None
+    if patch_id == last_update:
+        return None
+    else:
+        logging.info('new patch data found:',patch)
+        yaml.dump(patch_id,open('catabase/d2_patch_id.yaml','wb'))
+        return patch
+
 #returns last 25 matches for given user
 def findMatches(account_id):                                  
-    return api.get_match_history(account_id=account_id)["result"]["matches"]
-
+    try:
+        return api.get_match_history(account_id=account_id)["result"]["matches"]
+    except:
+        logging.error('no match history data found')
+        return logging.error('no match history found')
 
 #returns value for given key in results such as list of players, starttime etc
 #abstraction from api requests for simplicity, but currently experimenting with
 #using json  
 def getResults(match,key):
+    
     result = match['result']
     vals_list = result[key]
     return vals_list
@@ -56,22 +99,20 @@ def getSum(account_id,days,attribute):
                 results = getResults(match,'players')
                 attr_list.append(getPlayerVal(results,account_id,attribute))
             except requests.exceptions.HTTPError as e:
-                print "REQUEST ERROR:",str(e)
+                logging.error("request error:",str(e))
 
     return zip(match_ids,attr_list)    
 
 
-#To link telegram user with dota player. Any unique identifier from
-#telegram can be used.
-def get_dota_id_from_telegram(user_id):
+#To link telegram user with dota profile.
 
+def get_dota_id_from_telegram(user_id):
     spacecats = reg.getCats()
-    catmatch = filter(lambda cat: cat['first_name'] == user_id, spacecats)
+    catmatch = filter(lambda cat: cat['telegram_id'] == user_id, spacecats)
     return catmatch[0]['dota_id']
 
 #Returns the last match of a given player 
 def getLastMatch(dota_id):
-
     matches = findMatches(dota_id)
     match_id = (matches[0]["match_id"])
     return match_id
